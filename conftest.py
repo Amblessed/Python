0296e0300cdd03e2106440a40de0a4c3aefd776a
Okechukwu Bright Onwumere <brighthamza9@yahoo.com>
2025-07-17 13:01:56 +0200

    Added Headless Option

--- conftest.py
+++ conftest.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 @pytest.fixture
 def driver():
     options = webdriver.ChromeOptions()
+    options.add_argument("--headless")           # Run in headless mode
+    options.add_argument("--start-maximized")    # Maximize the window
     web_driver = webdriver.Chrome(options=options)
     yield web_driver
     web_driver.quit()
